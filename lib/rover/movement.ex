295c5db97c1d66c3c7df37c2f45863297466c204
Jan Fornoff <janfornoff@gmail.com>
2017-09-10 21:36:11 +0200

    Code formatting

--- lib/rover/movement.ex
+++ lib/rover/movement.ex
@@ -1,17 +1,17 @@
 defmodule Rover.Movement do
   def move_forward(%Rover.State{facing: "N"} = rover) do
-    %{ rover | x: rover.x + 1 }
+    %{rover | x: rover.x + 1}
   end
 
   def move_forward(%Rover.State{facing: "S"} = rover) do
-    %{ rover | x: rover.x - 1 }
+    %{rover | x: rover.x - 1}
   end
 
   def move_forward(%Rover.State{facing: "W"} = rover) do
-    %{ rover | y: rover.y - 1 }
+    %{rover | y: rover.y - 1}
   end
 
   def move_forward(%Rover.State{facing: "E"} = rover) do
-    %{ rover | y: rover.y + 1 }
+    %{rover | y: rover.y + 1}
   end
 end
